dc77f9108828e488c2497d4caa632b6c2467500f
Matt Haerle <matthaerle@gmail.com>
2017-05-22 19:49:19 -0500

    Test
    TEst

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 echo "<html>";
 echo "<head>
 		<title>Matts Test Page</title>
+		<link rel='shortcut icon' type='image/x-icon' href='https://storage.googleapis.com/homepagematt.appspot.com/Pictures/MisterChief.ico' />
 	</head>";
 echo "<body>
-		<img src='redditImage.jpg' alt='Hi' />
+		<img src='https://storage.googleapis.com/homepagematt.appspot.com/Pictures/redditImage.jpg' alt='Hi' />
 		</body>";
 echo "</html>"
 
