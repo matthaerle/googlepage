9fc7a5cb277854baae84dfe27910229a0634d393
Matt Haerle <matthaerle@gmail.com>
2017-05-24 11:11:29 -0500

    Icon

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "<html>";
 echo "<head>
 		<title>Matts Test Page</title>
-		<link rel='icon' type='image/x-icon' href='favicon.ico' />
+		<link rel='icon' type='image/x-icon' href='https://storage.googleapis.com/homepagematt.appspot.com/Pictures/favicon.ico' />
 	</head>";
 echo "<body>
 		<img src='https://storage.googleapis.com/homepagematt.appspot.com/Pictures/redditImage.jpg' alt='Hi' />
