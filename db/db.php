30cc816bf64a0669d3107584bfd8623d8b095ee7
Matt Haerle <matthaerle@gmail.com>
2017-07-26 12:15:53 -0500

    db

--- db/db.php
+++ db/db.php
@@ -4,7 +4,10 @@ $username = "homepage";
 $password = "hy9$@AKd88n@8";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new pdo('mysql:unix_socket=/cloudsql/homepagematt:us-central1:homepage;dbname=homepage',
+  $username,  // username
+  $password       // password
+  );
 
 // Check connection
 if ($conn->connect_error) {
