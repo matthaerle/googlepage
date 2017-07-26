a1ed7edec44a8f5d7c83b5d71bd03b895c7529aa
Matt Haerle <matthaerle@gmail.com>
2017-07-26 17:55:05 -0500

    test

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 
-<form action="links/add/" method="post">
+<form action="links/add" method="post">
 Link Text: <input type="text" name="name"><br>
 Link URL: <input type="text" name="url"><br>
     <?php
