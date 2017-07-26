4d74f3c1136e8083de2347a7ae2825c4f8109a11
Matt Haerle <matthaerle@gmail.com>
2017-07-26 18:04:38 -0500

    update

--- admin/add.php
+++ admin/add.php
@@ -3,6 +3,14 @@
  $url = $_POST["url"];
  $group = $_POST["group"];
  
- echo $name;
+ require '../db/db.php';
+ try {
+     $result = $conn->exec("insert into Tech_Support_Links(Link_Text,Link_URL,Group_ID) 
+     values ( '".$name ."','".$url."',".$group");");
+ } 
+ catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+$conn = null;
 
 ?>
