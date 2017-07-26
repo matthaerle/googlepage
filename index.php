1c05879c5dae273e6ad60610bf63b77fbd479605
Matt Haerle <matthaerle@gmail.com>
2017-07-26 12:25:02 -0500

    db

--- index.php
+++ index.php
@@ -35,6 +35,24 @@
     <?php
       require 'db/db.php'; 
     
+    class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+    
     try {
         $sql = "select * from Support_Link_Groups";
         $result = $conn->query($sql);
