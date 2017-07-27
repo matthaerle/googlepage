1bcb1607d1f75488b5c690b032f57f80986d737a
Matt Haerle <matthaerle@gmail.com>
2017-07-27 14:52:06 -0500

    test

--- index.php
+++ index.php
@@ -231,7 +231,7 @@
                         <div id="success"></div>
                         <div class="row">
                             <div class="form-group col-xs-12">
-                                <button type="submit" onclick="postStuff();" class="btn btn-success btn-lg">Send</button>
+                                <button onclick="postStuff();" class="btn btn-success btn-lg">Send</button>
                             </div>
                         </div>
                     </form>
