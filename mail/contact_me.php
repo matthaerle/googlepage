14a31fedc30f3bab5e6cde4a516ef6633144280e
Matt Haerle <matthaerle@gmail.com>
2017-07-27 12:57:22 -0500

    email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,12 +17,24 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
-// Create the email and send the message
+try {
+    $message = new Message();
+    $message->setSender('matthaerle4@gmail.com');
+    $message->addTo('matthaerle@gmail.com');
+    $message->setSubject($name.' Contact');
+    $message->setTextBody($message.'/n'.$phone.'/n'.$email_address);
+    $message->send();
+    echo 'Mail Sent';
+} catch (InvalidArgumentException $e) {
+    echo 'There was an error';
+}
+
+/*// Create the email and send the message
 $to = 'matthaerle4@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: matthaerle@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;	*/		
 ?>
