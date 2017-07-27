7f66dd14eb02769cea81ce41f15fcef7a6a15370
Matt Haerle <matthaerle@gmail.com>
2017-07-27 13:38:26 -0500

    mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,16 +1,5 @@
 <?php
 use \google\appengine\api\mail\Message;
-
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
 	
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
@@ -28,7 +17,7 @@ try {
 } catch (InvalidArgumentException $e) {
     echo 'There was an error';
 }
-
+echo "Hello";
 /*// Create the email and send the message
 $to = 'matthaerle4@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
