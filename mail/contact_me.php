fd5f620ab3bb6f7415dd50c8ee3e11fdb5c77d06
Matt Haerle <matthaerle@gmail.com>
2017-07-27 13:50:35 -0500

    email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,10 +1,10 @@
 <?php
 use google\appengine\api\mail\Message;
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = htmlspecialchars($_POST['name']);
+$email_address = htmlspecialchars($_POST['email']);
+$phone = htmlspecialchars($_POST['phone']);
+$message = htmlspecialchars($_POST['message']);
 	
 try {
     $message = new Message();
