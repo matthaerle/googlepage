7520b85b77c67141a3b9e9d4ba0e1286f29e7c69
Matt Haerle <matthaerle@gmail.com>
2017-08-01 10:21:22 -0500

    div

--- admin/index.php
+++ admin/index.php
@@ -14,26 +14,29 @@
 </head>
 <body>
 
-<form action="links/add" method="post">
-Link Text: <input type="text" name="name"><br>
-Link URL: <input type="text" name="url"><br>
-    <?php
-    require '../db/db.php';
-    try {
-        $result = $conn->query("select * from Support_Link_Groups");
-        echo '<select class="form-dropdown" id="group" name="group">';
-        foreach ($result as $row) {
-            echo '<option value="' . $row['Group_ID'] .'" > '. $row['Group_Name'] . '</option>';
-        }
-        echo '</select>';
-    }
-    catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-     ?>
-<input type="submit">
-</form>
+<div class="container">
+
+	<form action="links/add" method="post">
+	Link Text: <input type="text" name="name"><br>
+	Link URL: <input type="text" name="url"><br>
+	    <?php
+	    require '../db/db.php';
+	    try {
+	        $result = $conn->query("select * from Support_Link_Groups");
+	        echo '<select class="form-dropdown" id="group" name="group">';
+	        foreach ($result as $row) {
+	            echo '<option value="' . $row['Group_ID'] .'" > '. $row['Group_Name'] . '</option>';
+	        }
+	        echo '</select>';
+	    }
+	    catch (PDOException $e) {
+	        echo "Error: " . $e->getMessage();
+	    }
+	    $conn = null;
+	     ?>
+	<input type="submit">
+	</form>
+</div>
 
 </body>
 </html>
